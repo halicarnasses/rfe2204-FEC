1a165e1659a623426b72941d14c0009b4a701e11
Justin C Kirk <justin.c.kirk@gmail.com>
2022-05-23 14:30:33 -0400

    Questions now can make GET requests for data

--- client/components/Questions.jsx
+++ client/components/Questions.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import QuestionSearch from './questions/QuestionSearch.jsx';
 import QuestionList from './questions/QuestionList.jsx';
@@ -9,9 +9,23 @@ import './questions/Questions.css'
 function Questions({props}) {
   console.log(props);
   const [productID, setProductID] = useState(props);
-  const [questionData, setQuestionData] = useState();
+  const [questionData, setQuestionData] = useState([]);
+
+  const questionURL = '';
+
+
 
   // When componenet mounts, get question info for product.
+
+  useEffect(() => {
+    axios.get(`/qa/questions/?product_id=${productID}`)
+    .then((response) => {
+      console.log(response.data);
+      return setQuestionData(response.data.results);
+    });
+  }, [setQuestionData]);
+
+
 
   return (
     <div className="questions-div">

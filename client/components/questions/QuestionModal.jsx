9623999ae211574761097d8f22054ab0096f30a4
Justin C Kirk <justin.c.kirk@gmail.com>
2022-05-26 13:40:57 -0400

    implement question modal form draft

--- client/components/questions/QuestionModal.jsx
+++ client/components/questions/QuestionModal.jsx
@@ -4,11 +4,42 @@ import './QuestionModal.css'
 function QuestionModal ({show, hide, submitHandler}) {
   const showHideClassName = show ? 'modal display-block' : 'modal display-none';
 
+  const [newQuestion, setNewQuestoin] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+
+  const submitForm = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={showHideClassName}>
       <div className='modal-content'>
         <h1>Add Your Question!</h1>
         <button onClick={hide}>Close</button>
+
+        <form onSubmit={submitForm}>
+          {/*
+
+          Your Question: large text window allows up to 1000 chars
+          What Is Your Nickname: Text input up to 60 chars Placeholder= "Example: jackson11!"
+            Below line reads= "For privacy reasons, do not use your full name or email address"
+          Your Email: text input up to 60 chars
+            Below reads = "For authentication reasons, you will not be emailed"
+          Submit Question Button
+            When clicked, forms inputs are validated
+            If not correct, form does not submit
+            If not correct, message appears saying: You must enter the following
+            Error occurs if:
+              Fields are blank
+              email is not in correct format */}
+
+          <input type="text" placeholder="Your Question"></input>
+          <input type="text" placeholder="Your Nickname"></input>
+          <input type="text" placeholder="Your Email"></input>
+          <button type="submit">SUBMIT QUESTION</button>
+        </form>
+
       </div>
     </div>
   );

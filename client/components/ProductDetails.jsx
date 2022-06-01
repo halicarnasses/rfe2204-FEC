dc9c9809825c6fcc7ba077ed6065e74a771c261e
Justin C Kirk <justin.c.kirk@gmail.com>
2022-06-01 13:19:15 -0400

    commit before merge

--- client/components/ProductDetails.jsx
+++ client/components/ProductDetails.jsx
@@ -58,7 +58,7 @@ function ProductDetails() {
     <div>
       {/* <h1>Product Details</h1> */}
       <Overview id={productID} productInfo={productAll.productInfo} productStyles={productAll.productStyles} reviewsMeta={productAll.reviewsMeta} stateHandler={updateState}/>
-      <Questions id={productID} questionsData={productAll.questions} stateHandler={updateState}/>
+      {/* <Questions id={productID} questionsData={productAll.questions} stateHandler={updateState}/> */}
       {/* <Reviews id={productID} reviews={productAll.reviews} reviewsMeta={productAll.reviewsMeta} stateHandler={updateState}/> */}
     </div>
   );
